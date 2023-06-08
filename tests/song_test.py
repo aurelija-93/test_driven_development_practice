b5bc751466b58985d9d058cd8cd5f91f9be9e193
Aurelija Bellerby <aurelija.bellerby@gmail.com>
2023-06-08 13:51:03 +0100

    song and song_test complete

--- tests/song_test.py
+++ tests/song_test.py
@@ -5,4 +5,11 @@ class TestSong(unittest.TestCase):
     def setUp(self):
         self.song = Song("Dancing Queen", "Abba", "Euro-Pop")
 
-    
+    def test_song_has_name(self):
+        self.assertEqual("Dancing Queen", self.song.name)
+
+    def test_song_has_artist(self):
+        self.assertEqual("Abba", self.song.artist)
+
+    def test_song_has_genre(self):
+        self.assertEqual("Euro-Pop", self.song.genre)
